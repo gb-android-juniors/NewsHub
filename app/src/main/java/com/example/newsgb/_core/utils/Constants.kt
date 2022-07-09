6aca358314f4d6dc8114bb02da18160e788253c7
Kirill <bronskidev@gmail.com>
2022-07-09 10:14:59 +0300

    add constants class

---
+++ app/src/main/java/com/example/newsgb/_core/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsgb._core.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/v2/"
+        const val API_KEY = "689627db5cd24ea4a3567efa62a092e8"
+        const val TOP_HEADLINES = "top-headlines"
+    }
+}
