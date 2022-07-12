73fe64e3c2e53a91115b16716e818a09cfec05af
myasnikov.d <d.myasnikov@teledoc.ru>
2022-07-12 12:05:02 +0300

    refactoring

--- app/src/main/java/com/example/newsgb/bookmarks/ui/BookmarksFragment.kt
+++ app/src/main/java/com/example/newsgb/bookmarks/ui/BookmarksFragment.kt
@@ -13,7 +13,7 @@ class BookmarksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bookmarks, container, false)
+        return inflater.inflate(R.layout.bookmarks_fragment, container, false)
     }
 
     companion object {
