273f3c89594e21bb9242d5a0e1487c19336e5d84
Robilko <sasha.robson@gmail.com>
2022-07-13 19:43:46 +0300

    Реализован DAO

--- app/src/main/java/com/example/newsgb/_core/data/db/dao/BookmarkDao.kt
+++ app/src/main/java/com/example/newsgb/_core/data/db/dao/BookmarkDao.kt
@@ -1,7 +1,21 @@
 package com.example.newsgb._core.data.db.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.newsgb._core.data.db.entity.ArticleEntity
 
 @Dao
 interface BookmarkDao {
+
+    /** Получить весь список закладок*/
+    @Query("SELECT * FROM bookmark_table")
+    suspend fun all(): List<ArticleEntity>
+    /** Получить конкретную закладку */
+    @Query("SELECT * FROM bookmark_table WHERE id LIKE :id")
+    suspend fun getDataById(id: Int): ArticleEntity?
+    /** Сохранить новую закладку. onConflict = OnConflictStrategy.IGNORE означает, что дубликаты не будут сохраняться */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entity: ArticleEntity)
+    /** Удалить закладку */
+    @Delete
+    suspend fun delete(entity: ArticleEntity)
 }
