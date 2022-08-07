82e07ce352a00b75c1227a9370b0bc2015c573fb
Robilko <sasha.robson@gmail.com>
2022-08-08 00:44:23 +0300

    добавлен еще один режим темы приложения

--- app/src/main/java/com/example/newsgb/utils/ui/ThemeModes.kt
+++ app/src/main/java/com/example/newsgb/utils/ui/ThemeModes.kt
@@ -5,5 +5,6 @@ import com.example.newsgb.R
 enum class ThemeModes(val resIdName: Int) {
     SYSTEM_MODE(R.string.settings_system_theme_name),
     LIGHT_MODE(R.string.settings_light_theme_name),
-    DARK_MODE(R.string.settings_dark_theme_name)
+    DARK_MODE(R.string.settings_dark_theme_name),
+    BATTERY_MODE(R.string.settings_battery_theme_name)
 }
