a6bf4d0cda62df1dfb0517862c65dac1a720c6e6
Robilko <sasha.robson@gmail.com>
2022-08-07 12:56:08 +0300

    мелкий рефакторинг ApiService.kt

--- app/src/main/java/com/example/newsgb/utils/Constants.kt
+++ app/src/main/java/com/example/newsgb/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.example.newsgb.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org/v2/"
-        const val API_KEY = "689627db5cd24ea4a3567efa62a092e8"
         const val TOP_HEADLINES_ENDPOINT = "top-headlines"
         const val URL = "URL"
         const val APP_PREFERENCES_COUNTRY_CODE = "APP_PREFERENCES_COUNTRY_CODE"
