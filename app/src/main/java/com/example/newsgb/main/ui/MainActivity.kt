cc38ca7de98bef2ce931646b38904c636d6841b3
Robilko <sasha.robson@gmail.com>
2022-08-09 21:12:16 +0300

    эксперименты со сплэшем. Перенес его из активити во фрагмент, но не предсказуемо работает загрузка данных и отображение категорий

--- app/src/main/java/com/example/newsgb/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsgb/main/ui/MainActivity.kt
@@ -1,25 +1,17 @@
 package com.example.newsgb.main.ui
 
-import android.animation.ObjectAnimator
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager
-import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
-import android.view.ViewTreeObserver
-import android.view.animation.AnticipateInterpolator
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.animation.doOnEnd
 import com.example.newsgb.App
 import com.example.newsgb.R
 import com.example.newsgb._core.ui.store.NewsStore
 import com.example.newsgb._core.ui.store.NewsStoreHolder
 import com.example.newsgb.databinding.MainActivityBinding
-import com.example.newsgb.splash.ui.CustomSplashScreenActivity
+import com.example.newsgb.splash.ui.CustomSplashScreenFragment
 import com.example.newsgb.utils.Constants
 import com.example.newsgb.utils.PrivateSharedPreferences
 import com.example.newsgb.utils.network.OnlineLiveData
@@ -41,16 +33,14 @@ class MainActivity : AppCompatActivity(), NewsStoreHolder {
         setApplicationTheme()
         super.onCreate(savedInstanceState)
         setTheme(R.style.CustomThemeIndigo)
-        setSplashScreen()
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        startSplashScreen()
         //проверяем наличие интернет-подключения на старте
         isNetworkAvailable =
             (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo?.isConnected == true
         getCountryCodeFromPreferences()
         viewModel.getInitialData()
-        //запускаем главный фрагмент
-        startMainScreen()
         //подписываемся на изменение наличия интернет-подключения
         subscribeToNetworkChange()
     }
@@ -84,14 +74,14 @@ class MainActivity : AppCompatActivity(), NewsStoreHolder {
      * если интернет недоступен, уведомляем пользователя с помощью ImageView в активити и AlertDialog,
      * иначе запускаем основной фрагмент
      * */
-    private fun startMainScreen() {
+    private fun startSplashScreen() {
         if (!isNetworkAvailable && isDialogNull()) {
             showNoInternetConnectionInfo()
         } else if (isNetworkAvailable) {
             binding.networkLostImage.visibility = View.GONE
             binding.mainContainer.visibility = View.VISIBLE
             supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, NavigationFragment.newInstance())
+                .add(R.id.main_container, CustomSplashScreenFragment.newInstance())
                 .commit()
         }
     }
@@ -125,84 +115,11 @@ class MainActivity : AppCompatActivity(), NewsStoreHolder {
     private fun subscribeToNetworkChange() {
         OnlineLiveData(this).observe(this@MainActivity) {
             isNetworkAvailable = it
-            startMainScreen()
+            startSplashScreen()
         }
-    }
-
-    /**
-     * Устанавливает дефолтный SplashScreen если версия андроид 12 и выше, и кастомный SplashScreen,
-     * если у пользователя более ранняя версия андроид
-     * */
-    private fun setSplashScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            setDefaultSplashScreen()
-        } else {
-            setCustomSplashScreen()
-        }
-    }
-
-    private fun setCustomSplashScreen() {
-        startActivity(Intent(this@MainActivity, CustomSplashScreenActivity::class.java))
-    }
-
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun setDefaultSplashScreen() {
-        setSplashScreenHideAnimation()
-        setSplashScreenDuration()
-    }
-
-    /**
-     * Прописываем анимацию
-     * */
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun setSplashScreenHideAnimation() {
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            val slideLeft = ObjectAnimator.ofFloat(
-                splashScreenView,
-                View.TRANSLATION_X,
-                0f,
-                -splashScreenView.height.toFloat()
-            )
-            slideLeft.interpolator = AnticipateInterpolator()
-            slideLeft.duration = SLIDE_LEFT_DURATION
-            slideLeft.doOnEnd { splashScreenView.remove() }
-            slideLeft.start()
-        }
-    }
-
-    /**
-     * настройка времени отображения splash screen
-     * */
-    private fun setSplashScreenDuration() {
-        var isHideSplashScreen = false
-
-        object : CountDownTimer(
-            COUNTDOWN_DURATION, COUNTDOWN_INTERVAL
-        ) {
-            override fun onTick(millisUntilFinished: Long) {}
-            override fun onFinish() {
-                isHideSplashScreen = true
-            }
-        }.start()
-        val content: View = findViewById(android.R.id.content)
-        content.viewTreeObserver.addOnPreDrawListener(
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    return if (isHideSplashScreen) {
-                        content.viewTreeObserver.removeOnPreDrawListener(this)
-                        true
-                    } else {
-                        false
-                    }
-                }
-            }
-        )
     }
 
     companion object {
         const val DIALOG_FRAGMENT_TAG = "74a54328-5d62-46bf-ab6b-cbf5d8c79522"
-        private const val SLIDE_LEFT_DURATION = 1000L
-        private const val COUNTDOWN_DURATION = 2000L
-        private const val COUNTDOWN_INTERVAL = 1000L
     }
 }
--- app/src/main/java/com/example/newsgb/splash/ui/CustomSplashScreenFragment.kt
+++ app/src/main/java/com/example/newsgb/splash/ui/CustomSplashScreenFragment.kt
@@ -3,29 +3,33 @@ package com.example.newsgb.splash.ui
 import android.animation.Animator
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.view.animation.LinearInterpolator
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageView
 import com.example.newsgb.R
+import com.example.newsgb._core.ui.BaseFragment
+import com.example.newsgb.databinding.CustomSplashScreenFragmentBinding
+import com.example.newsgb.main.ui.NavigationFragment
 
 @SuppressLint("CustomSplashScreen")
-class CustomSplashScreenActivity : AppCompatActivity() {
+class CustomSplashScreenFragment : BaseFragment<CustomSplashScreenFragmentBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_custom_splash_screen)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setCustomSplashScreenAnimation()
     }
 
     private fun setCustomSplashScreenAnimation() {
-        findViewById<AppCompatImageView>(R.id.splash_image_view).animate()
+        binding.splashImageView.animate()
             .alpha(1f)
             .setInterpolator(LinearInterpolator())
             .setDuration(SPLASHSCREEN_ANIMATION_DURATION)
             .setListener(object : Animator.AnimatorListener {
 
                 override fun onAnimationEnd(animation: Animator?) {
-                    finish()
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .remove(this@CustomSplashScreenFragment)
+                        .add(R.id.main_container, NavigationFragment.newInstance())
+                        .commit()
                 }
 
                 override fun onAnimationCancel(animation: Animator?) {}
@@ -36,5 +40,10 @@ class CustomSplashScreenActivity : AppCompatActivity() {
 
     companion object {
         const val SPLASHSCREEN_ANIMATION_DURATION = 2000L
+        @JvmStatic
+        fun newInstance() = CustomSplashScreenFragment()
+
     }
+
+    override fun getViewBinding() = CustomSplashScreenFragmentBinding.inflate(layoutInflater)
 }
