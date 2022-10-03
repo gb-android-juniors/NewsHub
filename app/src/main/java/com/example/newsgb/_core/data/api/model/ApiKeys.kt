7baedd5eca0736f6f7041e1da23bd797e8f81fe1
Robilko <sasha.robson@gmail.com>
2022-10-03 14:10:24 +0300

    Добавил несколько ключей для API

--- app/src/main/java/com/example/newsgb/_core/data/api/model/ApiKeys.kt
+++ app/src/main/java/com/example/newsgb/_core/data/api/model/ApiKeys.kt
@@ -6,5 +6,8 @@ enum class ApiKeys (val token: String) {
     THIRD_TOKEN (token = "db300ae771fd44508376f909bafbc805"),
     FOURTH_TOKEN (token = "20f095f3271a43d98fe01c8bacab9769"),
     FIFTH_TOKEN (token = "37f0cf1abd7d43adb0557c1003cf1045"),
-    SIXTH_TOKEN (token = "0756a78eea4146cfa812f55a98f69e6c")
+    SIXTH_TOKEN (token = "0756a78eea4146cfa812f55a98f69e6c"),
+    SEVENTH_TOKEN (token = "ba8788d045394580bba1ef697cbcd0ee"),
+    EIGHTH_TOKEN (token = "f4f6f7ba556140f888d15ff714000d4d"),
+    NINTH_TOKEN (token = "0b91fdb7370243cb9d11f37ec21a3090")
 }
