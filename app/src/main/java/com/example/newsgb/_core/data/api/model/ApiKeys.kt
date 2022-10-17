4daff68640c97628762d1be381990ba692377dd0
Robilko <sasha.robson@gmail.com>
2022-10-18 00:01:37 +0300

    add tokens

--- app/src/main/java/com/example/newsgb/_core/data/api/model/ApiKeys.kt
+++ app/src/main/java/com/example/newsgb/_core/data/api/model/ApiKeys.kt
@@ -1,13 +1,29 @@
 package com.example.newsgb._core.data.api.model
 
-enum class ApiKeys (val token: String) {
-    FIRST_TOKEN (token = "689627db5cd24ea4a3567efa62a092e8"),
-    SECOND_TOKEN (token = "eea649ffccf84bacbaf3684c366c82c2"),
-    THIRD_TOKEN (token = "db300ae771fd44508376f909bafbc805"),
-    FOURTH_TOKEN (token = "20f095f3271a43d98fe01c8bacab9769"),
-    FIFTH_TOKEN (token = "37f0cf1abd7d43adb0557c1003cf1045"),
-    SIXTH_TOKEN (token = "0756a78eea4146cfa812f55a98f69e6c"),
-    SEVENTH_TOKEN (token = "ba8788d045394580bba1ef697cbcd0ee"),
-    EIGHTH_TOKEN (token = "f4f6f7ba556140f888d15ff714000d4d"),
-    NINTH_TOKEN (token = "0b91fdb7370243cb9d11f37ec21a3090")
+enum class ApiKeys(val token: String) {
+    A(token = "689627db5cd24ea4a3567efa62a092e8"),
+    B(token = "eea649ffccf84bacbaf3684c366c82c2"),
+    C(token = "db300ae771fd44508376f909bafbc805"),
+    D(token = "20f095f3271a43d98fe01c8bacab9769"),
+    F(token = "37f0cf1abd7d43adb0557c1003cf1045"),
+    G(token = "0756a78eea4146cfa812f55a98f69e6c"),
+    H(token = "ba8788d045394580bba1ef697cbcd0ee"),
+    I(token = "f4f6f7ba556140f888d15ff714000d4d"),
+    J(token = "0b91fdb7370243cb9d11f37ec21a3090"),
+    K(token = "9d7478a1503f4b8080082f63a23e7e61"),
+    L(token = "4be1f325622a4e19b75b5ef6762d1561"),
+    M(token = "38917e3f7c1f4e34bb040ea30f67172a"),
+    N(token = "145c8aa89d3843dab4e70ac6ab808515"),
+    O(token = "ead9d712f633418a9cfef71c77e38549"),
+    P(token = "ab136c2edbc44b98bc43009ba7db6e19"),
+    Q(token = "ea263571633f4ab98a429e95a6384a7f"),
+    R(token = "c3c197450fe846e7a1b50ae3da027706"),
+    S(token = "fe69d71bac17433a8499818ba99d8689"),
+    T(token = "ecdab20d0599402d9adc95da4ee12170"),
+    U(token = "72cda86f86124cc998d9127a629bbb5a"),
+    V(token = "7bfb87ccf7bd4fab9635f3b5e3bf2de4"),
+    W(token = "fe43252e7b664cd4badc32bfb116e6c7"),
+    X(token = "6916688761a3492099bfa9ba150c996b"),
+    Y(token = "b55b7eac10a44be5b6414145fccacc8f"),
+    Z(token = "25d2d867dcf74843ac7fed3366546b37")
 }
